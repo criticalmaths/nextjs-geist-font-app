e82aaf94df566fdc8b8ffc08cacbdb97f1ce2f7d
criticalmaths <sforestdfw@icloud.com>
2025-07-21 06:43:20 -0500

    Add BlackBoxAI.VSExtension/Properties/AssemblyInfo.cs 1753098199589

---
+++ BlackBoxAI.VSExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BlackBox AI Visual Studio Extension")]
+[assembly: AssemblyDescription("AI-powered coding assistant for Visual Studio 2022")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BlackBox AI")]
+[assembly: AssemblyProduct("BlackBox AI Visual Studio Extension")]
+[assembly: AssemblyCopyright("Copyright © 2024 BlackBox AI")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
